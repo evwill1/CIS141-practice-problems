07866bb9626bf2fb1a9fe628066bf76478429c13
Evan Williams <92429177+evwill1@users.noreply.github.com>
2025-01-29 14:49:51 -0800

    first commit

--- mod4-problems.py
+++ mod4-problems.py
@@ -1,12 +1,11 @@
 #1. Construct a truth table for the expression: (A AND B) OR (NOT B) where A and B each can be True or False. Your truth table should be commented out (as it's not valid Python code!)
 
-#AND operator &&
-#A      B       A AND B
-#-------------------------
-#true   True    True
-#true   False   False
-#False  True    False
-#False  False   False
+# A  B    (A AND B)    (NOT B)    (A AND B) OR (NOT B)
+#----------------------------------------------------
+# T  T      T           F           T
+# F  F      F           T           T
+# T  F      F           T           T
+# F  T      F           F           F
 
 #2. The headlights of a car should only automatically turn on when the daylight outside is below a certain threshold. If a sensor threshold is below the number 8, print "Headlights On"; otherwise, print "Headlights Off".
 
